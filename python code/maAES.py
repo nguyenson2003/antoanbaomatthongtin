3e4d7a3862e930051fd6cb15a629d7c13159937a
LeVaNmInH1703 <105649952+LeVaNmInH1703@users.noreply.github.com>
2024-03-27 02:30:27 +0700

    done

--- python code/maAES.py
+++ python code/maAES.py
@@ -19,10 +19,9 @@ s_box=[["63","7C","77","7B","F2","6B","6F","C5","30","01","67","2B","FE","D7","A
        ["E1","F8","98","11","69","D9","8E","94","9B","1E","87","E9","CE","55","28","DF"],
        ["8C","A1","89","0D","BF","E6","42","68","41","99","2D","0F","B0","54","BB","16"]
        ]
-#theo đầu bài thì k là ...
+#theo đầu bài thì key là ...
 # k="CDAB0FC51CACBCF9A8A348C3D2D0247A"
-#k ví dụ của thầy
-k="2b7e151628aed2a6abf7158809cf4f3c"
+
 #hàm chia khóa k thành 4 phần
 def div_k_into_4_word(k):
     if len(k)!=32:
@@ -59,11 +58,15 @@ def hex_xor(hex1, hex2):
         raise ValueError("hexa invalid")
     return hex(int(hex1, 16)^int(hex2, 16))[2:].zfill(max(len(hex1), len(hex2)))
 
-
+#
+#k ví dụ của thầy
+key="2b7e151628aed2a6abf7158809cf4f3c"
+#m là plaintext
+m="3243f6a8885a308d313198a2e0370734"
 #mảng w
 w=[None]*50
 #Chia khóa K (128 bit) thành 4 word (32 bit)
-w[0],w[1],w[2],w[3]=div_k_into_4_word(k)
+w[0],w[1],w[2],w[3]=div_k_into_4_word(key)
 #Dịch vòng trái 1 byte đối với w3 (32 bit)
 rw=RotWord(w[3])
 #Thay thế từng byte trong rw bằng bảng S-box SubWord
@@ -77,7 +80,8 @@ w[6]=hex_xor(w[5],w[2])
 w[7]=hex_xor(w[6],w[3])
 
 #mảng k
-k=[w[4]+w[5]+w[6]+w[7]]
+k=[w[0]+w[1]+w[2]+w[3]]
+k.append(w[4]+w[5]+w[6]+w[7])
 #LẶP LẠI từ Bài 2 đến Bài 5 để tạo các khóa K2, K3, ..., K10
 for i in range(2,11):
     rw=RotWord(w[i*4-1])
@@ -89,3 +93,82 @@ for i in range(2,11):
     w[i*4+3]=hex_xor(w[i*4+2],w[i*4-1])
     k.append(w[i*4]+w[i*4+1]+w[i*4+2]+w[i*4+3])
 # nếu w43=b6630ca6 thì đã sinh 10 khóa thành công 
+
+
+
+#hàm addroundkey thực chất là xor_hexa
+def AddRoundKey(hex1, hex2):
+    return hex_xor(hex1,hex2)
+
+#mảng dịch vòng trái 0,5,10,15,4,9,14,3,8,13,2,7,12,1,6,11
+#hàm dịch vòng trái đối với plaintext
+def shift_rows(state=""):
+    res=""
+    #mảng trên
+    for i in [0,5,10,15,
+              4,9,14,3,
+              8,13,2,7,
+              12,1,6,11]:
+        res=res+state[i*2:i*2+2]
+    return res
+#hàm nhân 2 hexa rồi mod cho ma trận mod trả về dec
+def hex_mul(hex1,hex2):
+    if not all(char in "0123456789ABCDEFabcdef" for char in hex1+hex2):
+        raise ValueError("hexa invalid")
+    #bin->dec
+    mod=int("100011011",2)
+    #hex->dec
+    hex1=int(hex1,16)
+    hex2=int(hex2,16)
+    res=0
+    for i in range(hex1.bit_length()):
+        if hex1>>i&1 :
+            res ^= hex2<<i
+    while res.bit_length()>=9:
+        res^=mod<<(res.bit_length()-9)
+    return res
+#hàm mix_columns
+def mix_columns(state):
+    default_matrix = [
+        "02", "03", "01", "01",
+        "01", "02", "03", "01",
+        "01", "01", "02", "03",
+        "03", "01", "01", "02"
+    ]
+    #xoay chiều hàng thành cột cột thành hàng của state
+    state_matrix= [state[i*2:i*2+2] for i in [0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15]]
+    result=""
+    for i in [0,4,8,12,1,5,9,13,2,6,10,14,3,7,11,15]:
+        # hàng của ma trận cố định, cột của ma trận trạng thái
+        row=int(i/4)
+        col=i%4
+        temp=0
+        temp^=hex_mul(default_matrix[row*4+0],state_matrix[4*0+col])
+        temp^=hex_mul(default_matrix[row*4+1],state_matrix[4*1+col])
+        temp^=hex_mul(default_matrix[row*4+2],state_matrix[4*2+col])
+        temp^=hex_mul(default_matrix[row*4+3],state_matrix[4*3+col])
+        result+=hex(temp)[2:].zfill(2)
+        
+    return result.upper()
+
+#Tính kết quả AddRoundKey
+state= AddRoundKey(m,k[0])
+#Thay thế từng byte trong state bằng bảng S-box SubByte
+state=replaceS_box(state)
+#Dịch vòng trái các byte trong state ShiftRows
+state = shift_rows(state)
+#Trộn các byte trong state MixColumns
+state = mix_columns (state)
+# print(state)
+#Dịch vòng trái các byte trong state AddRoundKey
+state = AddRoundKey(state, k[1])
+#===================== 8 VÒNG LẶP ===========
+for i in range(2,10):
+    print(state,k[i])
+    state=replaceS_box(state)
+    state = shift_rows(state)
+    state = mix_columns (state)
+    state = AddRoundKey(state, k[i])
+#Vòng lặp cuối (lần lặp 10) 
+C = state = AddRoundKey(shift_rows( replaceS_box(state)), k[10])
+print(C)
--- python code/test.py
+++ python code/test.py
@@ -1,4 +1,1 @@
-def hex_xor(hex1, hex2):
-    if not all(char in "0123456789ABCDEFabcdef" for char in hex1+hex2):
-        raise ValueError("hexa invalid")
-hex_xor("1234567890abcdef","bcdefdg")
+print(all(char in '1234' for char in "12345"))
