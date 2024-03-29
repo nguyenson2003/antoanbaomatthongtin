d4b96d23c394aaadd86fcc5f4cefbbbb0632b881
nqson <nqson123@gmail.com>
2024-03-29 21:47:46 +0700

    fix vigenere

--- Chuong1_macodien/macodien_bai3.py
+++ Chuong1_macodien/macodien_bai3.py
@@ -1,6 +1,10 @@
-def caesar_encrypt(text, key):# m,k
+def vigenere_autokey_encrypt(text, key):# m,k
     return "".join([chr((ord(i)+ord(j)-ord('A')-ord('A'))%26+ord('A')) for i,j in zip(text,key+text)])
 
-#câu 1
+def vigenere_autokey_decrypt(text, key):# m,k
+    res = []
+    for i in range(len(text)):
+        res.append( chr((ord(text[i])-ord(key[i] if i<len(key) else res[i-len(key)])-ord('A')-ord('A')+26*26)%26+ord('A')))
+    return res
 
-print(caesar_encrypt("THETRUTHWILLO","THEGRASS"))
+print(vigenere_autokey_encrypt("THETRUTHWILLO","THEGRASS"))
